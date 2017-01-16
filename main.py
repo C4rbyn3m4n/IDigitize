b4bc52cf9816fb72af536b88a72693806316da86
qwerty0981 <qwerty.0981@gmail.com>
2017-01-16 18:54:20 -0500

    Restructured code to allow for a more object oriented approach

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter as tk
 class mainWindow():
     def __init__(self, master):
         self.master = master
+
         self.frameMain = tk.Frame(self.master)
         self.buttonSignIn = tk.Button(self.frameMain, height=13, width=55, text="Sign In", command=self.commandSignIn)
         self.buttonSignIn.pack()
@@ -12,22 +13,40 @@ class mainWindow():
 
         self.frameMain.pack()
 
+        #Variable Declaration
+        self.varSignIn = tk.StringVar(self.frameMain)
+        self.varID = tk.StringVar(self.frameMain)
+        self.varName = tk.StringVar(self.frameMain)
+
+
     def commandSignIn(self):
         pass
 
     def commandSignOut(self):
-        self.varName = tk.StringVar(self.frameMain)
+
         self.varName.set("Name")
-        self.varID = tk.StringVar(self.frameMain)
+
         self.varID.set("ID")
-        self.varSignIn = tk.StringVar(self.frameMain)
+
         self.varSignIn.set("Please swipe your student ID card")
         self.frameMain.pack_forget()
 
-        self.frameSignOut = tk.Frame(self.master)
-        tk.Label(self.frameSignOut, text=self.varSignIn.get()).pack()
-        tk.Entry(self.frameSignOut, text=self.varName, justify="center").pack()
-        tk.Entry(self.frameSignOut, text=self.varID, justify="center").pack()
+        self.signoutWindow = windowSignOut(self)
+
+class windowSignIn():
+    def __init__(self, parent):
+        self.parent = parent
+
+
+
+class windowSignOut():
+    def __init__(self, parent):
+        self.parent = parent
+
+        self.frameSignOut = tk.Frame(self.parent.master)
+        tk.Label(self.frameSignOut, text=self.parent.varSignIn.get()).pack()
+        tk.Entry(self.frameSignOut, text=self.parent.varName, justify="center").pack()
+        tk.Entry(self.frameSignOut, text=self.parent.varID, justify="center").pack()
         self.frameSignOutButtons = tk.Frame(self.frameSignOut)
         tk.Button(self.frameSignOutButtons, text="Submit", command=self.commandSubmit).pack(side=tk.LEFT)
         tk.Button(self.frameSignOutButtons, text="Cancel", command=self.commandCancel).pack(side=tk.RIGHT)
@@ -44,15 +63,12 @@ class mainWindow():
 
     def commandCancel(self):
         self.frameSignOut.destroy()
-        self.frameMain.pack()
-
+        self.parent.frameMain.pack()
 
     def getSwipe(self):
         # while not self.test:
         #     pass
         return True
-
-
 
 
 
