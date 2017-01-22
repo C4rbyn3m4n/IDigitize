cd8bb87d8412c4c890aabf3b48da2916d960a439
C4rbyn3m4n <hanumanthu.saivamsi@gmail.com>
2017-01-22 13:53:38 -0500

    Changed UI

--- MainClass/windowScan.py
+++ MainClass/windowScan.py
@@ -13,22 +13,25 @@ class windowScan():
 
         # makes a frame on tk window
         self.mframe = tk.Frame(self.swipe)
+
+
         # makes string variable that can be set
         self.var = tk.StringVar(self.mframe)
         self.CardLabel = tk.Label(self.mframe, text="Scan your card")
         self.CardLabel.pack()
+
+        self.frameButtons = tk.Frame(self.mframe)
+        self.buttonOkay = tk.Button(self.frameButtons, text="OK", command=self.okay, height=3, width=10)
+        self.buttonOkay.pack(side=tk.LEFT)
+        self.swipe.bind('<Return>', lambda event: self.okay())
+        self.frameButtons.pack()
+
+        tk.Button(self.frameButtons, text="Cancel",height=3, width=10, command=lambda: self.swipe.destroy()).pack(side=tk.RIGHT)
+
         self.mEntry = tk.Entry(self.mframe, text=self.var)
         self.mEntry.pack()
         self.mEntry.focus()
 
-        self.frameButtons = tk.Frame(self.mframe)
-        self.buttonOkay = tk.Button(self.frameButtons, text="OK", command=self.okay)
-        self.buttonOkay.pack(side=tk.LEFT)
-        self.swipe.bind('<Return>', lambda event: self.okay())
-
-        tk.Button(self.frameButtons, text="Cancel", command=lambda: self.swipe.destroy()).pack(side=tk.RIGHT)
-
-        self.frameButtons.pack()
         self.mframe.pack()
         self.swipe.mainloop()
 
