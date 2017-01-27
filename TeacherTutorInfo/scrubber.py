c6109e755e978e524113f45fe413db16e1d6ba8b
C4rbyn3m4n <hanumanthu.saivamsi@gmail.com>
2017-01-27 13:51:31 -0500

    Scrubber Code

--- TeacherTutorInfo/scrubber.py
+++ TeacherTutorInfo/scrubber.py
@@ -1,3 +1,7 @@
-TeachersHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Teachers.txt";
-TutorsHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Tutors.txt";
+import requests
 
+teachersHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Teachers.txt";
+tutorsHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Tutors.txt";
+
+teacherData= requests.get(teachersHTML).text
+print (teacherData)
