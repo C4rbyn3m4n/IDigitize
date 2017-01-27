7874852d957d0ce5cc4b1db3faec523d5967a93c
C4rbyn3m4n <hanumanthu.saivamsi@gmail.com>
2017-01-27 14:01:40 -0500

    Scrubber Code

--- TeacherTutorInfo/scrubber.py
+++ TeacherTutorInfo/scrubber.py
@@ -3,5 +3,7 @@ import requests
 teachersHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Teachers.txt";
 tutorsHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Tutors.txt";
 
-teacherData= requests.get(teachersHTML).text
-print (teacherData)
+tutorsData = requests.get(tutorsHTML).text
+
+for line in tutorsData:
+    print(line)
