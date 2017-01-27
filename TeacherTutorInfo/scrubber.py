534c652f29748ebe8958259f63eff210ce7e3855
C4rbyn3m4n <hanumanthu.saivamsi@gmail.com>
2017-01-27 13:47:46 -0500

    Scrubber Code

---
+++ TeacherTutorInfo/scrubber.py
@@ -0,0 +1,3 @@
+TeachersHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Teachers.txt";
+TutorsHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Tutors.txt";
+
