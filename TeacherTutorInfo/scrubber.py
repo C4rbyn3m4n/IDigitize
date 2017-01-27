11b449371951630e0010b6c86a55f901769efd6a
C4rbyn3m4n <hanumanthu.saivamsi@gmail.com>
2017-01-27 14:31:49 -0500

    Scrubber Code

--- TeacherTutorInfo/scrubber.py
+++ TeacherTutorInfo/scrubber.py
@@ -1,9 +1,21 @@
 import requests
 
+classAndTacher = []
+info = []
+
+
 teachersHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Teachers.txt";
 tutorsHTML = "https://raw.githubusercontent.com/C4rbyn3m4n/IDigitize/master/TeacherTutorInfo/Tutors.txt";
 
-tutorsData = requests.get(tutorsHTML).text
+teacherData = requests.get(teachersHTML).text
+lines = teacherData.split('\n')
 
-for line in tutorsData:
-    print(line)
+for line in lines:
+    if ""==line:
+        classAndTacher.append(info)
+        info = []
+    else:
+        info.append(line)
+
+
+print(classAndTacher)
