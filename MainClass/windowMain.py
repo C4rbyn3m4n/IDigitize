5ef76cc8fab72a894e1e0572102f751950a29120
qwerty0981 <qwerty.0981@gmail.com>
2017-01-30 15:19:56 -0500

    FInal push

--- MainClass/windowMain.py
+++ MainClass/windowMain.py
@@ -9,17 +9,22 @@ class windowMain():
     def __init__(self, master):
         # make parent accessible for all methods
         self.master = master
+        #loads the teacher/class array from the server
         self.loadClassTeachers()
 
-
-        # create sign-in and sign-out buttons
+        # create the main frame for the object
         self.frameMain = tk.Frame(self.master)
 
+        # format the window size to fill the screen completely
         self.master.geometry("{0}x{1}+0+0".format(master.winfo_screenwidth(), master.winfo_screenheight()))
+        # make escape key reduce window size for debugging
         self.master.bind('<Escape>', lambda event: self.master.geometry('{}x{}'.format(500, 500)))
+        # binds Control+Shift+t to shutdown the server
         self.master.bind('<Control-T>', lambda event: self.master.destroy())
+        # prevents the 'x' button from destroying the window
         self.master.protocol('WM_DELETE_WINDOW', self.doNothing)
 
+        # TODO: Add background image
         # self.imageBackground = tk.PhotoImage("FloridaPolyGray.png")
         # self.labelBackgroundImage = tk.Label(self.frameMain, image=self.imageBackground)
         # self.labelBackgroundImage.place(x=0, y=0, relwidth=1, relheight=1)
@@ -31,41 +36,46 @@ class windowMain():
         self.buttonSignOut = tk.Button(self.frameMain, height=13, width=55, text="Sign Out", command=self.commandSignOut)
         self.buttonSignOut.pack()
 
+        # packs the main frame  to show the screen
         self.frameMain.pack()
 
         # Variable Declaration
-        self.varSignIn = tk.StringVar(self.frameMain)
-        self.varID = tk.StringVar(self.frameMain)
-        self.varName = tk.StringVar(self.frameMain)
+        self.varSignIn = tk.StringVar(self.frameMain)       # stores Sign-in message
+        self.varID = tk.StringVar(self.frameMain)           # Stores the input ID
+        self.varName = tk.StringVar(self.frameMain)         # Stores the input Name
+        self.varFinalName = tk.StringVar(self.frameMain)    # Stores finalized value of Name for submission
+        self.varFinalID = tk.StringVar(self.frameMain)      # Stores finalized value of ID for submission
 
-        self.varFinalName = tk.StringVar(self.frameMain)
-        self.varFinalID = tk.StringVar(self.frameMain)
-
+        # sets the default value for these variables (What you see in the entry boxes)
         self.varName.set("Name")
         self.varID.set("ID")
         self.varSignIn.set("Please swipe your student ID card")
 
-
+    # Function called when sign-in button is pressed
     def commandSignIn(self):
         # hides the main class buttons
         self.frameMain.pack_forget()
 
+        # opens the next window
         self.signinWindow = windowSignIn.windowSignIn(self)
 
+    # Function called when the sign-out button is pressed
     def commandSignOut(self):
-        # set up variables for the Sign out window
-
         # hide the main class buttons
         self.frameMain.pack_forget()
 
+        # opens the sign-out window
         self.signoutWindow = windowSignOut.windowSignOut(self)
 
+    # Function to load teacher/class array from server
     def loadClassTeachers(self):
+        # Trys to load teachers if the server does not respond then repeat the call after 1 second
         try:
             self.arrayClassTeachers = client().getClassteacherArray()
         except UnboundLocalError as e:
             self.master.after(1000, self.loadClassTeachers)
 
+    # used to prevent the 'x' button from doing anything
     def doNothing(self):
         pass
 
--- MainClass/windowSignOut.py
+++ MainClass/windowSignOut.py
@@ -1,3 +1,4 @@
+# imports required modules
 import tkinter as tk
 
 import windowScan
@@ -7,19 +8,34 @@ from client import client
 
 class windowSignOut():
     def __init__(self, parent):
+        # Stores the parent to allow accessing the previous object variables
         self.parent = parent
 
+        # Makes the main frame to store widgets
         self.frameSignOut = tk.Frame(self.parent.master)
+
+        # Makes the label for the sign-in text
         tk.Label(self.frameSignOut, text=self.parent.varSignIn.get()).pack()
+
+        # Makes the entries for the Name and ID (These will be filled by the card scan)
         tk.Entry(self.frameSignOut, text=self.parent.varName, justify="center").pack()
         tk.Entry(self.frameSignOut, text=self.parent.varID, justify="center").pack()
+
+        # Makes a seperate frame for the buttons to be stored on to allow better formatting
         self.frameSignOutButtons = tk.Frame(self.frameSignOut)
+
+        # Makes both Submit and Cancel buttons and assigns their commands
         tk.Button(self.frameSignOutButtons, text="Submit", command=self.commandSubmit).pack(side=tk.LEFT)
         tk.Button(self.frameSignOutButtons, text="Cancel", command=self.commandCancel).pack(side=tk.RIGHT)
+
+        # Binds the enter key to the submit method
         self.parent.master.bind('<Return>', lambda event: self.commandSubmit())
+
+        # Packs both of the frames
         self.frameSignOutButtons.pack()
         self.frameSignOut.pack()
 
+        # Opens a scan window to get card info
         self.ScanWindow = windowScan.windowScan(self)
 
     def commandSubmit(self):
